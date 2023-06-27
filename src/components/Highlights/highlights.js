062e047b4b75ea12ffff4af22dcf4b3a4fd70040
Sidharth Menon <sidharthm3non99@gmail.com>
2023-06-28 02:08:05 +0530

    Changes made

--- src/components/Highlights/highlights.js
+++ src/components/Highlights/highlights.js
@@ -68,6 +68,71 @@ export default function Highlights() {
                         <CardContent sx={{ display: 'flex', position: 'relative' }}>
                             <Grid container spacing={5}>
                                 <Grid item xs={1} sx={{ marginLeft: '1%' }}>
+                                    <Typography sx={{ fontWeight: 'bold', fontSize: '14px' }}>9987</Typography>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Typography sx={{ fontWeight: 'bold', fontSize: '14px', marginLeft: '4%' }}>Depp v Heard</Typography>
+                                </Grid>
+                                <Grid item xs={2}>
+                                    <Typography sx={{ fontWeight: 'bold', fontSize: '14px', marginLeft: '4%' }}>Johnny Depp</Typography>
+                                </Grid>
+                                <Grid item xs={2}>
+                                    <Typography sx={{ fontWeight: 'bold', fontSize: '14px', marginLeft: '4%' }}>01-June-2022</Typography>
+                                </Grid>
+                                <Grid item xs={2}>
+                                    <Typography sx={{ fontWeight: 'bold', fontSize: '14px', marginLeft: '4%' }}>Settled</Typography>
+                                </Grid>
+                                <Grid item xs={1}>
+                                    <Typography sx={{ fontWeight: 'bold', fontSize: '14px', marginLeft: '4%' }}>$10 million</Typography>
+                                </Grid>
+                            </Grid>
+                            <IconButton
+                                aria-label="expand row"
+                                size="small"
+                                onClick={() => setFourthOpen(!openFourth)}
+                                sx={{ position: 'absolute', right: 5, marginTop: '-0.3%', backgroundColor: 'white' }}
+                            >
+                                {openFourth ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                            </IconButton>
+                        </CardContent>
+                        <Collapse in={openFourth} timeout="auto" unmountOnExit>
+                            <CardContent>
+                                <Grid container spacing={5}>
+                                    <Grid item xs={1} sx={{ marginLeft: '1%' }}>
+                                    </Grid>
+                                    <Grid item xs={3}>
+                                        <Typography sx={{ fontSize: '14px', marginLeft: '4%' }}>On June 1, 2022, after nearly two days of deliberations, the jury found that Depp
+                                         had proven all the elements of defamation for all three statements from Heard's 2018 op-ed, including that the statements were false, and 
+                                         that Heard defamed Depp with actual malice. The jury awarded Depp $10 million in compensatory damages and $5 million in punitive damages 
+                                         from Heard. The punitive damages, however, were reduced to $350,000 due to a limit imposed by Virginia state law.
+                                         In regard to Heard's counterclaim, the jury found the second of the three contested statements that Depp's former lawyer Adam Waldman had 
+                                         published in the Daily Mail to be defamatory and false, defaming Heard with actual malice. Regarding the other two contested statements, 
+                                         the jurors concluded that Heard's attorneys had not proven all the elements of defamation. Heard was awarded $2 million in compensatory 
+                                         damages from Depp but no punitive damages.</Typography>
+                                    </Grid>
+                                    <Grid item xs={2}>
+                                        <Typography sx={{ fontWeight: 'bold', fontSize: '14px', marginLeft: '4%' }}>vs.</Typography>
+                                        <br /><Typography sx={{ fontWeight: 'bold', fontSize: '14px', marginLeft: '4%' }}>Amber Heard</Typography>
+                                    </Grid>
+                                    <Grid item xs={2}>
+                                    </Grid>
+                                    <Grid item xs={2}>
+                                        <Typography sx={{
+                                            fontSize: '14px', marginLeft: '4%', backgroundColor: 'white', borderRadius: '14px',
+                                            paddingLeft: '15px'
+                                        }}>Helped Johnny clear his name and win $10 million in damages</Typography>
+                                    </Grid>
+                                    <Grid item xs={1}>
+                                    </Grid>
+                                </Grid>
+                            </CardContent>
+                        </Collapse>
+                    </Card>
+                    <br />
+                    <Card elevation={0} sx={{ backgroundColor: '#DBDFEA', borderRadius: '10px', height: '100%' }}>
+                        <CardContent sx={{ display: 'flex', position: 'relative' }}>
+                            <Grid container spacing={5}>
+                                <Grid item xs={1} sx={{ marginLeft: '1%' }}>
                                     <Typography sx={{ fontWeight: 'bold', fontSize: '14px' }}>1555</Typography>
                                 </Grid>
                                 <Grid item xs={3}>
@@ -246,69 +311,6 @@ export default function Highlights() {
                                             fontSize: '14px', marginLeft: '4%', backgroundColor: 'white', borderRadius: '14px',
                                             paddingLeft: '15px'
                                         }}>Helped someone with little knowledge find a favourable outcome in this case.</Typography>
-                                    </Grid>
-                                    <Grid item xs={1}>
-                                    </Grid>
-                                </Grid>
-                            </CardContent>
-                        </Collapse>
-                    </Card>
-                    <br />
-                    <Card elevation={0} sx={{ backgroundColor: '#DBDFEA', borderRadius: '10px', height: '100%' }}>
-                        <CardContent sx={{ display: 'flex', position: 'relative' }}>
-                            <Grid container spacing={5}>
-                                <Grid item xs={1} sx={{ marginLeft: '1%' }}>
-                                    <Typography sx={{ fontWeight: 'bold', fontSize: '14px' }}>1555</Typography>
-                                </Grid>
-                                <Grid item xs={3}>
-                                    <Typography sx={{ fontWeight: 'bold', fontSize: '14px', marginLeft: '4%' }}>Garg Builders v Bharat Heavy Electricals</Typography>
-                                </Grid>
-                                <Grid item xs={2}>
-                                    <Typography sx={{ fontWeight: 'bold', fontSize: '14px', marginLeft: '4%' }}>Garg Builders</Typography>
-                                </Grid>
-                                <Grid item xs={2}>
-                                    <Typography sx={{ fontWeight: 'bold', fontSize: '14px', marginLeft: '4%' }}>25-April-2023</Typography>
-                                </Grid>
-                                <Grid item xs={2}>
-                                    <Typography sx={{ fontWeight: 'bold', fontSize: '14px', marginLeft: '4%' }}>Settled</Typography>
-                                </Grid>
-                                <Grid item xs={1}>
-                                    <Typography sx={{ fontWeight: 'bold', fontSize: '14px', marginLeft: '4%' }}>₹80,000</Typography>
-                                </Grid>
-                            </Grid>
-                            <IconButton
-                                aria-label="expand row"
-                                size="small"
-                                onClick={() => setFourthOpen(!openFourth)}
-                                sx={{ position: 'absolute', right: 5, marginTop: '-0.3%', backgroundColor: 'white' }}
-                            >
-                                {openFourth ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                            </IconButton>
-                        </CardContent>
-                        <Collapse in={openFourth} timeout="auto" unmountOnExit>
-                            <CardContent>
-                                <Grid container spacing={5}>
-                                    <Grid item xs={1} sx={{ marginLeft: '1%' }}>
-                                    </Grid>
-                                    <Grid item xs={3}>
-                                        <Typography sx={{ fontSize: '14px', marginLeft: '4%' }}>In the recent case of Garg Builders v Bharat Heavy Electricals Limited [Civil Appeal
-                                            No. 6261 of 2021], a two-judge bench of the Supreme Court of India (Supreme Court) held that an arbitrator cannot grant pendente lite
-                                            interest when the contracting parties have freely and expressly opted out of receiving interest under the contract.
-                                            The Supreme Court's verdict holds special significance since (i) it analysed the scope of the interest barring clause under the contract
-                                            between the parties vis-à-vis Section 28 of the Indian Contract Act, 1872 (Contract Act); and (ii) it reinforced the well-founded principle
-                                            that an arbitrator is a creature of contract, and its powers cannot traverse beyond the purview of the contract.</Typography>
-                                    </Grid>
-                                    <Grid item xs={2}>
-                                        <Typography sx={{ fontWeight: 'bold', fontSize: '14px', marginLeft: '4%' }}>vs.</Typography>
-                                        <br /><Typography sx={{ fontWeight: 'bold', fontSize: '14px', marginLeft: '4%' }}>Bharat Heavy Electricals</Typography>
-                                    </Grid>
-                                    <Grid item xs={2}>
-                                    </Grid>
-                                    <Grid item xs={2}>
-                                        <Typography sx={{
-                                            fontSize: '14px', marginLeft: '4%', backgroundColor: 'white', borderRadius: '14px',
-                                            paddingLeft: '15px'
-                                        }}>Helped in resolving this case in record time.</Typography>
                                     </Grid>
                                     <Grid item xs={1}>
                                     </Grid>
